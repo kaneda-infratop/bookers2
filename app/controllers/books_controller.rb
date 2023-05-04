e60bf9df6aa14c742a93c3fc6da95e7528388366
EC2 Default User <ec2-user@ip-172-31-41-101.ap-northeast-1.compute.internal>
2023-05-04 16:15:59 +0000

    destroy ok

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,16 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to "/books/#{@book.id}"
+    else
+      render :new
+    end
   end
 
   def index
@@ -20,9 +24,15 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title,:body,:image)
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,12 @@
 class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
-
+  
+  with_options presence: true do
+    validates :title
+    validates :body
+  end
+  
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
